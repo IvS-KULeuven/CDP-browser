d8f8a637c24d2d699765fbf864a49b91041604c9
Wim De Meester <wim.demeester@ster.kuleuven.be>
2014-06-17 10:27:43 +0200

    Reworked tables and pager.

--- cdp/list.php
+++ cdp/list.php
@@ -1,5 +1,5 @@
 <?php
-global $cdpLocation;
+global $cdpLocation, $objUtil;
 
 // We make some tabs, to see the different CDP releases
 
@@ -19,12 +19,7 @@ if (!$conn_id) {
 	echo " </ul>";
 
 	// Add the button for the columns
-	echo "   <div class=\"columnSelectorWrapper\">
-              <input id=\"colSelect1\" type=\"checkbox\" class=\"hidden\">
-              <label class=\"columnSelectorButton\" for=\"colSelect1\">Column</label>
-              <div id=\"columnSelector\" class=\"columnSelector\">
-              </div>
-	         </div>";
+	$objUtil->addTableColumSelector();
 	
 	echo " <div id=\"my-tab-content\" class=\"tab-content\">
 	        <div class=\"tab-pane active\" id=\"cdp3\">";
@@ -65,29 +60,8 @@ if (!$conn_id) {
 	ftp_close($conn_id);
 
 	echo "    </tbody>
-			  <tfoot>
-			    <tr>
-                 <th colspan=\"3\" class=\"ts-pager form-horizontal\">
-                 <button type=\"button\" class=\"btn first\"><i class=\"icon-step-backward glyphicon glyphicon-step-backward\"></i>
-                 </button>
-                 <button type=\"button\" class=\"btn prev\"><i class=\"icon-arrow-left glyphicon glyphicon-backward\"></i>
-                 </button>	<span class=\"pagedisplay\"></span> 
-                 <!-- this can be any element, including an input -->
-                 <button type=\"button\" class=\"btn next\"><i class=\"icon-arrow-right glyphicon glyphicon-forward\"></i>
-                 </button>
-                 <button type=\"button\" class=\"btn last\"><i class=\"icon-step-forward glyphicon glyphicon-step-forward\"></i>
-                 </button>
-                 <select class=\"pagesize input-mini\" title=\"Select page size\">
-                     <option selected=\"selected\" value=\"10\">10</option>
-                     <option value=\"20\">20</option>
-                     <option value=\"30\">30</option>
-                     <option value=\"40\">40</option>
-                 </select>
-                 <select class=\"pagenum input-mini\" title=\"Select page number\"></select>";
-    echo "       </th>
-                </tr>
-               </tfoot>			
 			 </table>";
+	echo $objUtil->addTablePager();
 	echo "  </div>";
 //           <div class=\"tab-pane\" id=\"cdp2\">
 //             <h1>CDP 2 release</h1>
@@ -100,54 +74,7 @@ if (!$conn_id) {
    	echo "  </div>
    		   </div><br /><br />";
 
-   	// Make the table sorter, add the pager and add the column chooser
-   	echo "<script type=\"text/javascript\">";
-   	echo "$(\"table\").tablesorter({
-            theme: \"bootstrap\",
-            widthFixed: true,
-            headerTemplate: '{content} {icon}',
-            widgets: [\"uitheme\", \"columnSelector\", \"filter\", \"zebra\"],
-            widgetOptions : {
-              // target the column selector markup
-              columnSelector_container : $('#columnSelector'),
-              // column status, true = display, false = hide
-              // disable = do not display on list
-              columnSelector_columns : {
-                0: 'disable' /* set to disabled; not allowed to unselect it */
-              },
-              // remember selected columns (requires $.tablesorter.storage)
-              columnSelector_saveColumns: true,
-
-              // container layout
-              columnSelector_layout : '<label><input type=\"checkbox\">{name}</label>',
-              // data attribute containing column name to use in the selector container
-              columnSelector_name  : 'data-selector-name',
-
-             /* Responsive Media Query settings */
-             // enable/disable mediaquery breakpoints
-             columnSelector_mediaquery: true,
-             // toggle checkbox name
-             columnSelector_mediaqueryName: 'Auto: ',
-             // breakpoints checkbox initial setting
-             columnSelector_mediaqueryState: true,
-             // responsive table hides columns with priority 1-6 at these breakpoints
-             // see http://view.jquerymobile.com/1.3.2/dist/demos/widgets/table-column-toggle/#Applyingapresetbreakpoint
-             // *** set to false to disable ***
-             columnSelector_breakpoints : [ '20em', '30em', '40em', '50em', '60em', '70em' ],
-             // data attribute containing column priority
-             // duplicates how jQuery mobile uses priorities:
-             // http://view.jquerymobile.com/1.3.2/dist/demos/widgets/table-column-toggle/
-             columnSelector_priority : 'data-priority'
-            }
-          })
-          .tablesorterPager({
-            container: $(\".ts-pager\"),
-            cssGoto: \".pagenum\",
-            output: '{startRow} - {endRow} / {filteredRows} ({totalRows})'
-          });";
-   			
-   	echo "</script>";
-   	
+   	$objUtil->addTableJavascript();
 }
 
 ?>
--- common/content/user_admin.php
+++ common/content/user_admin.php
@@ -1,23 +1,17 @@
 <?php
-  global $objUser, $baseURL;
+  global $objUser, $baseURL, $objUtil;
   
   // We create an array with all users
   $users = $objUser->getUsers();
   
   echo "<div class=\"container-fluid\">";
-  // Add the button for the columns
-  echo "   <div class=\"columnSelectorWrapper\">
-              <input id=\"colSelect1\" type=\"checkbox\" class=\"hidden\">
-              <label class=\"columnSelectorButton\" for=\"colSelect1\">Column</label>
-              <div id=\"columnSelector\" class=\"columnSelector\">
-              </div>
-	         </div>";
-  
+  $objUtil->addTableColumSelector();
+
   // Add a button to add a new user
   echo "<button type=\"button\" class=\"btn btn-success pull-right\" data-toggle=\"modal\" data-target=\"#addUser\">
   		 <span class=\"glyphicon glyphicon-plus\"></span>&nbsp;Add a new user
   		</button>";	
-  
+ 
   // We make a table with all files from the ftp site
   echo "   <table class=\"table table-striped table-hover tablesorter custom-popup\">";
   echo "    <thead>
@@ -37,36 +31,16 @@
 	  		echo "<td>" . $value['name'] . "</td>";
 	  		echo "<td>" . $value['email'] . "</td>";
 	  		echo "<td>" . $value['role'] . "</td>";
-	  		echo "<td><span class=\"glyphicon glyphicon-download\"></span></td>";
+	  		echo "<td><span class=\"glyphicon glyphicon-trash\"></span></td>";
 	        echo "</tr>\n";
   }
   echo "    </tbody>
-			  <tfoot>
-			    <tr>
-                 <th colspan=\"6\" class=\"ts-pager form-horizontal\">
-                 <button type=\"button\" class=\"btn first\"><i class=\"icon-step-backward glyphicon glyphicon-step-backward\"></i>
-                 </button>
-                 <button type=\"button\" class=\"btn prev\"><i class=\"icon-arrow-left glyphicon glyphicon-backward\"></i>
-                 </button>	<span class=\"pagedisplay\"></span>
-                 <!-- this can be any element, including an input -->
-                 <button type=\"button\" class=\"btn next\"><i class=\"icon-arrow-right glyphicon glyphicon-forward\"></i>
-                 </button>
-                 <button type=\"button\" class=\"btn last\"><i class=\"icon-step-forward glyphicon glyphicon-step-forward\"></i>
-                 </button>
-                 <select class=\"pagesize input-mini\" title=\"Select page size\">
-                     <option selected=\"selected\" value=\"10\">10</option>
-                     <option value=\"20\">20</option>
-                     <option value=\"30\">30</option>
-                     <option value=\"40\">40</option>
-                 </select>
-                 <select class=\"pagenum input-mini\" title=\"Select page number\"></select>";
-  echo "       </th>
-                </tr>
-               </tfoot>
-			 </table>";
+  		   </table>";
+  echo $objUtil->addTablePager();
+
   echo "  </div>";
   
-  // Setting the log in modal form
+  // Setting the add new user modal form
   echo "<div class=\"modal fade\" id=\"addUser\" tabindex=\"-1\" role=\"dialog\" aria-hidden=\"true\">
           <div class=\"modal-dialog\">
             <div class=\"modal-content\">
@@ -91,52 +65,5 @@
         </div>
   	  </body>";
   
-  // Make the table sorter, add the pager and add the column chooser
-  echo "<script type=\"text/javascript\">";
-  echo "$(\"table\").tablesorter({
-            theme: \"bootstrap\",
-            widthFixed: true,
-            headerTemplate: '{content} {icon}',
-            widgets: [\"uitheme\", \"columnSelector\", \"filter\", \"zebra\"],
-            widgetOptions : {
-              // target the column selector markup
-              columnSelector_container : $('#columnSelector'),
-              // column status, true = display, false = hide
-              // disable = do not display on list
-              columnSelector_columns : {
-                0: 'disable' /* set to disabled; not allowed to unselect it */
-              },
-              // remember selected columns (requires $.tablesorter.storage)
-              columnSelector_saveColumns: true,
-  
-              // container layout
-              columnSelector_layout : '<label><input type=\"checkbox\">{name}</label>',
-              // data attribute containing column name to use in the selector container
-              columnSelector_name  : 'data-selector-name',
-  
-             /* Responsive Media Query settings */
-             // enable/disable mediaquery breakpoints
-             columnSelector_mediaquery: true,
-             // toggle checkbox name
-             columnSelector_mediaqueryName: 'Auto: ',
-             // breakpoints checkbox initial setting
-             columnSelector_mediaqueryState: true,
-             // responsive table hides columns with priority 1-6 at these breakpoints
-             // see http://view.jquerymobile.com/1.3.2/dist/demos/widgets/table-column-toggle/#Applyingapresetbreakpoint
-             // *** set to false to disable ***
-             columnSelector_breakpoints : [ '20em', '30em', '40em', '50em', '60em', '70em' ],
-             // data attribute containing column priority
-             // duplicates how jQuery mobile uses priorities:
-             // http://view.jquerymobile.com/1.3.2/dist/demos/widgets/table-column-toggle/
-             columnSelector_priority : 'data-priority'
-            }
-          })
-          .tablesorterPager({
-            container: $(\".ts-pager\"),
-            cssGoto: \".pagenum\",
-            output: '{startRow} - {endRow} / {filteredRows} ({totalRows})'
-          });";
-  
-  echo "</script>";
-  
+  $objUtil->addTableJavascript();
 ?>
