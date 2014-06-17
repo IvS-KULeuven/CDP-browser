b2610087aed3e3f0de283c710c5f491146e06552
Wim De Meester <wim.demeester@ster.kuleuven.be>
2014-06-17 14:27:01 +0200

    Printing the metadata key and the value.

--- lib/fits.php
+++ lib/fits.php
@@ -18,9 +18,17 @@ class Fits
             // This loops over the header, till we reach END in the fits file.
             $buffer = fgets($handle, 81);
             // Process buffer here..
-            print "<br />\n" .$buffer;
             if (strpos($buffer, "END") === 0) {
               $end = true;
+            } else {
+              $fitsValue = explode("=", $buffer);
+              $keyword = explode(" ", $fitsValue[0])[0];
+              $value = rtrim(ltrim(explode("/", $fitsValue[1])[0]));
+
+              $value = str_replace("'", "", $value);
+
+              // Check in the database which keywords can be used and only add this metadata to the array to return
+              print "<br />\nKeyword : |" .$keyword . "| - Value : |" . $value . "|";
             }
           }
 
