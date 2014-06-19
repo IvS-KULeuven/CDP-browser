340ffa30897b383f1db43064c22bfb76325a1107
Wim De Meester <wim.demeester@ster.kuleuven.be>
2014-06-19 10:43:01 +0200

    Show all metadata names in a table.

--- metadata/content/metadata_admin.php
+++ metadata/content/metadata_admin.php
@@ -1,10 +1,49 @@
 <?php
-  global $objDatabase, $objMetadata;
+  global $objDatabase, $objMetadata, $objUtil;
+
+  // We display a table with all known metadata keywords and possible values
+  echo "<div class=\"container-fluid\">";
+  
+  // Add the button for the columns
+  $objUtil->addTableColumSelector();
+  
+  
+  
+  
+  // We make a table with all cdp keywords
+  echo "   <table class=\"table table-striped table-hover tablesorter custom-popup\">";
+  echo "    <thead>
+             <th data-priority=\"critical\">Metadata name</th>
+             <th data-priority=\"2\">Type</th>
+             <th data-priority=\"2\">Possible values</th>
+      <th class=\"filter-false columnSelector-disable\" data-sorter=\"false\">Action</th>
+            </thead>";
+  echo "    <tbody>";
+  
+  $metadata = $objMetadata->getKeys();
+  
+  foreach ($metadata as $key => $value) {
+    echo "<tr>";
+    echo "<td>" . $value[0] . "</td>";
+    
+//           echo "<td>". $key . "</td>";
+//           echo "<td>" . $value['size'] . "</td>";
+//           echo "<td><span class=\"glyphicon glyphicon-download\"></span></td>";
+    echo "</tr>\n";
+  }
+
+  echo "    </tbody>
+			 </table>";
+  
+  echo $objUtil->addTablePager();
+  
+  $objUtil->addTableJavascript();
+  
+  echo "</div>";
   
   // We just print all the metadata types :
-  print_r($objMetadata->getKeys());
   
-  // We also print the possible values for DETECTOR
-  print "<br />Possible DETECTOR values : <br />";
-  print_r($objMetadata->getValidValues("DETECTOR"));
+//   // We also print the possible values for DETECTOR
+//   print "<br />Possible DETECTOR values : <br />";
+//   print_r($objMetadata->getValidValues("DETECTOR"));
 ?>
