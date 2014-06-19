ec83c415db55f50e8589538a4cb2bb05e46b5650
Wim De Meester <wim.demeester@ster.kuleuven.be>
2014-06-19 10:24:26 +0200

    Return the list of known parameters from the header of the fits file.

--- lib/fits.php
+++ lib/fits.php
@@ -2,40 +2,46 @@
 // fits.php
 // The fits class collects all functions needed to read the metadata from fits files.
 
-$fits = new Fits();
-$fits->getHeader("/lhome/wim/Downloads/MIRI_FM_IM_F1000W_PSF_02.00.01.fits");
-
 class Fits
 {
-	public function getHeader($filename) {
-		echo "Reading in : " . $filename;
+  public function getHeader($filename) {
+    global $objMetadata;
 		
-		$handle = fopen($filename, "r");
-		
-		if($handle) {
-		  $end = false;
-          while (!$end) {
-            // This loops over the header, till we reach END in the fits file.
-            $buffer = fgets($handle, 81);
-            // Process buffer here..
-            if (strpos($buffer, "END") === 0) {
-              $end = true;
-            } else {
-              $fitsValue = explode("=", $buffer);
-              $keyword = explode(" ", $fitsValue[0])[0];
-              $value = rtrim(ltrim(explode("/", $fitsValue[1])[0]));
+    $handle = fopen($filename, "r");
 
-              $value = str_replace("'", "", $value);
+    $toReturn = array(); 
+    
+    if($handle) {
+      $end = false;
+      $keys = $objMetadata->getKeys();
 
-              // Check in the database which keywords can be used and only add this metadata to the array to return
-              print "<br />\nKeyword : |" .$keyword . "| - Value : |" . $value . "|";
+      while (!$end) {
+        // This loops over the header, till we reach END in the fits file.
+        $buffer = fgets($handle, 81);
+        // Process buffer here..
+        if (strpos($buffer, "END") === 0) {
+          $end = true;
+        } else {
+          $fitsValue = explode("=", $buffer);
+          $keyword = explode(" ", $fitsValue[0])[0];
+          $value = rtrim(ltrim(explode("/", $fitsValue[1])[0]));
+
+          $value = str_replace("'", "", $value);
+
+          // Check in the database which keywords can be used and only add this metadata to the array to return
+          foreach ($keys as $key) {
+            if ($keyword == $key["id"]) {
+              $toReturn[$keyword] = $value;
             }
           }
+        }
+      }
 
-          fclose($handle);
-		} else {
-          $entryMessage = "Problem reading fits file!";
-		}
-	}
+      fclose($handle);
+    } else {
+     $entryMessage = "Problem reading fits file!";
+    }
+    return $toReturn;
+  }
 }
 ?>
--- metadata/content/metadata_admin.php
+++ metadata/content/metadata_admin.php
@@ -1,10 +1,16 @@
 <?php
-  global $objDatabase, $objMetadata;
+  global $objDatabase, $objMetadata, $baseURL;
   
   // We just print all the metadata types :
-  print_r($objMetadata->getKeys());
+//  print_r($objMetadata->getKeys());
   
   // We also print the possible values for DETECTOR
-  print "<br />Possible DETECTOR values : <br />";
-  print_r($objMetadata->getValidValues("DETECTOR"));
+//   print "<br />Possible DETECTOR values : <br />";
+//   print_r($objMetadata->getValidValues("DETECTOR"));
+  
+  require_once "lib/fits.php";
+   $fits = new Fits();
+   $keywords = $fits->getHeader("/lhome/wim/Downloads/MIRI_FM_IM_F1000W_PSF_02.00.01.fits");
+
+   print_r($keywords);
 ?>
