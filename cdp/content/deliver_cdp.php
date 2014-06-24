995bd85674195e221afa41203ce54299608082eb
Wim De Meester <wim.demeester@ster.kuleuven.be>
2014-06-24 15:17:31 +0200

    Also show dates of the files on the ftp directory.

--- cdp/content/deliver_cdp.php
+++ cdp/content/deliver_cdp.php
@@ -10,14 +10,32 @@ $objUtil->addTableColumSelector();
 
 // We make a table with all files from the ftp site
 echo "   <table class=\"table table-striped table-hover tablesorter custom-popup\">";
-echo "    <thead><th data-priority=\"critical\">Filename</th><th data-priority=\"2\">Size</th><th class=\"filter-false columnSelector-disable\" data-sorter=\"false\">Action</th></thead>";
+echo "    <thead>
+           <th data-priority=\"critical\">Filename</th>
+           <th data-priority=\"2\">Date</th>
+           <th data-priority=\"4\">Size</th>
+           <th class=\"filter-false columnSelector-disable\" data-sorter=\"false\">Action</th>
+          </thead>";
 echo "    <tbody>";
 
 $items = $objCdp->getFilesFromFtpServer();
 
 foreach ($items as $key => $value) {
+//[time] => 2013 [day] => 3 [month] => Sep
+//[time] => 09:28 [day] => 4 [month] => Mar
+
+  // Getting the date from the information
+  $year = $value["time"];
+  if (strpos($year, ":")) {
+    $year = date("Y");
+  }
+  $date = $year . "-";
+  $date .= date('m',strtotime($value["month"])) . "-";
+  $date .= sprintf("%02d", $value["day"]);
+
   echo "<tr>";
   echo "<td>". $key . "&nbsp;<span class=\"pull-right badge\">CDP 1</span>&nbsp;<span class=\"pull-right badge alert-success\">CDP 2</span>&nbsp;</td>"; 
+  echo "<td>" . $date . "</td>";
   echo "<td>" . $value['size'] . "</td>";
   echo "<td><span class=\"glyphicon glyphicon-download\"></span></td>";
   echo "</tr>\n";
