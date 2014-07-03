c56818ed59d0c5ab3ae119cd130d6aa20f270734
Wim De Meester <wim.demeester@ster.kuleuven.be>
2014-07-03 11:03:02 +0200

    Keywords from fits files are now also added to the database.

--- cdp/control/deliver_cdp_file.php
+++ cdp/control/deliver_cdp_file.php
@@ -4,13 +4,19 @@ global $objCdp, $objFits, $ftp_server;
 $filename = $_POST['filename'];
 $delivery = $_POST['DELIVERY'];
 
-print_r($objFits->getHeader($filename));
-//exit;
 $objCdp->deliver_file($filename, $delivery);
 
 // Add all the other keys
 foreach ($_POST as $key => $value) {
-  if ($key != "indexAction") {
+  if ($key != "indexAction" && $key != "filename") {
+    $objCdp->addKey($filename, $key, $value);
+  }
+}
+
+$fitsKeywords = $objFits->getHeader($filename);
+
+foreach($fitsKeywords as $key => $value) {
+  if ($key != "FILENAME") {
     $objCdp->addKey($filename, $key, $value);
   }
 }
