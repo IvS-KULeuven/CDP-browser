ab9dcf2e75e1d41a9f61cf90be9d83783b934b05
Wim De Meester <wim.demeester@ster.kuleuven.be>
2014-11-04 13:20:05 +0100

    Added newlines before resetting the failed variable.

--- miri_cdp_delivery.bash.php
+++ miri_cdp_delivery.bash.php
@@ -78,7 +78,8 @@ fi
             $fileNames = $objCdp->getFileNamesCdp ( $delivery [0], $module, $step, $refType, $fileType );
             // If there are files, we make a directory for this combination and download the files
             if (count ( $fileNames ) > 0) {
-              echo "failed=0
+              echo "
+  failed=0
   cd \$cdpdir/CDP" . $delivery [0] . "/" . $module . "/" . $step . "/" . $refType . "/" . $fileType;
               echo "
   echo \"Checking files in \$cdpdir/CDP" . $delivery [0] . "/" . $module . "/" . $step . "/" . $refType . "/" . $fileType . "\"";
@@ -135,12 +136,14 @@ fi
               $ftype = $ft [0];
               foreach ( $restStep as $ste ) {
                 if ($ste [2] == "") {
-                  echo "failed=0
+                  echo "
+  failed=0
   cd \$cdpdir/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ftype [2] . "/" . "\n";
                   echo "
   echo \"Checking files in \$cdpdir/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ftype [2] . "\"";
                 } else {
-                  echo "failed=0
+                  echo "
+  failed=0
   cd \$cdpdir/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ste [2] . "/" . $ftype [2] . "/" . "\n";
                   echo "
   echo \"Checking files in \$cdpdir/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ste [2] . "/" . $ftype [2] . "\"";
@@ -170,7 +173,8 @@ fi
             } else {
               $ft = $objCdp->getProperty ( $item, "FILETYPE" );
               $ftype = $ft [0];
-              echo "failed=0
+              echo "
+  failed=0
   cd \$cdpdir/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ftype [2] . "/" . "\n";
               echo "
   echo \"Checking files in \$cdpdir/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ftype [2] . "\"";
