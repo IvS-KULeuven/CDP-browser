b66180b8d788745242b584f6b86df779e9b39a88
Wim De Meester <wim.demeester@ster.kuleuven.be>
2015-07-09 12:13:55 +0200

    Fix some problems with escaping the cdpdir.

--- miri_cdp_delivery.bash.php
+++ miri_cdp_delivery.bash.php
@@ -126,87 +126,86 @@ fi
         }
       }
     }
-    echo "TEST";
-    // Now we may have still some files over. We put them at the correct location.
-    if (sizeof ( $newItems ) > 0) {
-      foreach ( $newItems as $item ) {
-        $restModules = $objCdp->getProperty ( $item, "PIPELINE_MODULE" );
-        if (sizeof ( $restModules ) > 0) {
-          foreach ( $restModules as $modu ) {
-            $restStep = $objCdp->getProperty ( $item, "PIPELINE_STEP" );
-            if (sizeof ( $restStep ) > 0) {
-              $ft = $objCdp->getProperty ( $item, "FILETYPE" );
-              $ftype = $ft [0];
-              foreach ( $restStep as $ste ) {
-                if ($ste [2] == "") {
-                  echo "
-  failed=0
-  cd \"\$cdpdir\"/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ftype [2] . "/" . "\n";
-                  echo "
-  echo \"Checking files in \$cdpdir/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ftype [2] . "\"";
-                } else {
-                  echo "
-  failed=0
-  cd \"\$cdpdir\"/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ste [2] . "/" . $ftype [2] . "/" . "\n";
-                  echo "
-  echo \"Checking files in \$cdpdir/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ste [2] . "/" . $ftype [2] . "\"";
-                }
+//     // Now we may have still some files over. We put them at the correct location.
+//     if (sizeof ( $newItems ) > 0) {
+//       foreach ( $newItems as $item ) {
+//         $restModules = $objCdp->getProperty ( $item, "PIPELINE_MODULE" );
+//         if (sizeof ( $restModules ) > 0) {
+//           foreach ( $restModules as $modu ) {
+//             $restStep = $objCdp->getProperty ( $item, "PIPELINE_STEP" );
+//             if (sizeof ( $restStep ) > 0) {
+//               $ft = $objCdp->getProperty ( $item, "FILETYPE" );
+//               $ftype = $ft [0];
+//               foreach ( $restStep as $ste ) {
+//                 if ($ste [2] == "") {
+//                   echo "
+//   failed=0
+//   cd \"\$cdpdir\"/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ftype [2] . "/" . "\n";
+//                   echo "
+//   echo \"Checking files in \$cdpdir/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ftype [2] . "\"";
+//                 } else {
+//                   echo "
+//   failed=0
+//   cd \"\$cdpdir\"/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ste [2] . "/" . $ftype [2] . "/" . "\n";
+//                   echo "
+//   echo \"Checking files in \$cdpdir/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ste [2] . "/" . $ftype [2] . "\"";
+//                 }
                 
-                echo "\n  file=\"" . $item . "\"
-  if [[ -e \$file ]] ; then
-    md5v=`grep \"" . $item . "\" md5_miri_cdps | uniq`
-    if [ -n \"\$md5v\" ] ; then
-      md5v=`echo \$md5v | awk '{if(NF != 2){print \"0\"} else {print \$1}}'`
-    else
-      md5v=\"1\"
-    fi
-    if [ \"\$md5v\" == \"1\" ]; then 
-      echo \"\$file NO MD5 HASH\"
-    else
-      if [ `md5_value \$file` != \$md5v ] ; then
-        echo \"\$file FAILED\"
-        failed=1
-      fi
-    fi
-  else
-    echo \"\$file does not exist\"
-    failed=1
-  fi";
-              }
-            } else {
-              $ft = $objCdp->getProperty ( $item, "FILETYPE" );
-              $ftype = $ft [0];
-              echo "
-  failed=0
-  cd \"\$cdpdir\"/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ftype [2] . "/" . "\n";
-              echo "
-  echo \"Checking files in \$cdpdir/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ftype [2] . "\"";
+//                 echo "\n  file=\"" . $item . "\"
+//   if [[ -e \$file ]] ; then
+//     md5v=`grep \"" . $item . "\" md5_miri_cdps | uniq`
+//     if [ -n \"\$md5v\" ] ; then
+//       md5v=`echo \$md5v | awk '{if(NF != 2){print \"0\"} else {print \$1}}'`
+//     else
+//       md5v=\"1\"
+//     fi
+//     if [ \"\$md5v\" == \"1\" ]; then 
+//       echo \"\$file NO MD5 HASH\"
+//     else
+//       if [ `md5_value \$file` != \$md5v ] ; then
+//         echo \"\$file FAILED\"
+//         failed=1
+//       fi
+//     fi
+//   else
+//     echo \"\$file does not exist\"
+//     failed=1
+//   fi";
+//               }
+//             } else {
+//               $ft = $objCdp->getProperty ( $item, "FILETYPE" );
+//               $ftype = $ft [0];
+//               echo "
+//   failed=0
+//   cd \"\$cdpdir\"/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ftype [2] . "/" . "\n";
+//               echo "
+//   echo \"Checking files in \$cdpdir/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ftype [2] . "\"";
               
-              echo "\n  file=\"" . $item . "\"
-  if [[ -e \$file ]] ; then
-    md5v=`grep \"" . $item . "\" md5_miri_cdps | uniq`
-    if [ -n \"\$md5v\" ] ; then
-      md5v=`echo \$md5v | awk '{if(NF != 2){print \"0\"} else {print \$1}}'`
-    else
-      md5v=\"1\"
-    fi
-    if [ \"\$md5v\" == \"1\" ]; then 
-      echo \"\$file NO MD5 HASH\"
-    else
-      if [ `md5_value \$file` != \$md5v ] ; then
-        echo \"\$file FAILED\"
-        failed=1
-      fi
-    fi
-  else
-    echo \"\$file does not exist\"
-    failed=1
-  fi";
-            }
-          }
-       }
-     }
-   }
+//               echo "\n  file=\"" . $item . "\"
+//   if [[ -e \$file ]] ; then
+//     md5v=`grep \"" . $item . "\" md5_miri_cdps | uniq`
+//     if [ -n \"\$md5v\" ] ; then
+//       md5v=`echo \$md5v | awk '{if(NF != 2){print \"0\"} else {print \$1}}'`
+//     else
+//       md5v=\"1\"
+//     fi
+//     if [ \"\$md5v\" == \"1\" ]; then 
+//       echo \"\$file NO MD5 HASH\"
+//     else
+//       if [ `md5_value \$file` != \$md5v ] ; then
+//         echo \"\$file FAILED\"
+//         failed=1
+//       fi
+//     fi
+//   else
+//     echo \"\$file does not exist\"
+//     failed=1
+//   fi";
+//             }
+//           }
+//        }
+//     }
+//   }
   }
   echo "
 }
@@ -327,7 +326,7 @@ cd \"\$cdpdir\"";
         
         echo "
         HOST=\"" . $ftp_user . ":" . $ftp_password . "@" . $ftp_server . "\"
-        LCD=\"\\\"\$cdpdir\"" . "/CDP" . $delivery [0] . "/" . $key . "\"
+        LCD=\"\\\"\$cdpdir\\\"" . "/CDP" . $delivery [0] . "/" . $key . "\"
         RCD=\"$ftp_directory\"
         
         lftp -c \"set ftp:list-options -a;
@@ -390,7 +389,7 @@ echo \"mirror --verbose \\\\\"              >> lftp_script";
               
               echo "
                   HOST=\"" . $ftp_user . ":" . $ftp_password . "@" . $ftp_server . "\"
-                  LCD=\"\\\"\$cdpdir\"" . "/CDP" . $delivery [0] . "/" . $module . "/" . $step . "/" . $refType . "/" . $fileType . "\"
+                  LCD=\"\\\"\$cdpdir\\\"" . "/CDP" . $delivery [0] . "/" . $module . "/" . $step . "/" . $refType . "/" . $fileType . "\"
                   RCD=\"$ftp_directory\"
               
                   lftp -c \"set ftp:list-options -a;
@@ -444,9 +443,9 @@ echo \"mirror --verbose \\\\\"              >> lftp_script";
               $ftype = $ft [0];
               foreach ( $restStep as $ste ) {
                 if ($ste [2] == "") {
-                  $dir = "\\\"\$cdpdir\"/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ftype [2] . "/";
+                  $dir = "\\\"\$cdpdir\\\"/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ftype [2] . "/";
                 } else {
-                  $dir = "\\\"\$cdpdir\"/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ste [2] . "/" . $ftype [2] . "/";
+                  $dir = "\\\"\$cdpdir\\\"/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ste [2] . "/" . $ftype [2] . "/";
                 }
                 echo "mkdir -p " . $dir . "\n";
                 
@@ -485,7 +484,7 @@ echo \"mirror --verbose \\\\\"              >> lftp_script";
             } else {
               $ft = $objCdp->getProperty ( $item, "FILETYPE" );
               $ftype = $ft [0];
-              $dir = "\\\"\$cdpdir\"/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ftype [2] . "/";
+              $dir = "\\\"\$cdpdir\\\"/CDP" . $delivery [0] . "/" . $modu [2] . "/" . $ftype [2] . "/";
               
               echo "mkdir -p " . $dir . "\n";
               
